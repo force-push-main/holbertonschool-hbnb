1bd45185e32f9cc596eea134343ee0aaaa0bbeea
force-push-main <10535@holbertonstudents.com>
2025-06-16 16:42:18 +1000

    saving broken mess for later

--- hbnb/app/models/review.py
+++ hbnb/app/models/review.py
@@ -3,6 +3,7 @@ from app.models.base import BaseModel
 class Review(BaseModel):
     def __init__(self, text=None, rating=None, place=None, user=None):
         super().__init__()
+
         self.text = text
         self.rating = rating
         self.place = place
