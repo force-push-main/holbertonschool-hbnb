f8ec7ba9906c17ae406dfa7808ea065e62cbf5c6
adashp02 <adashp02@gmail.com>
2025-08-08 13:46:22 +1000

    update2

--- hbnb/app/models/place.py
+++ hbnb/app/models/place.py
@@ -4,7 +4,6 @@ from app.persistence.repository import db
 
 class Place(BaseModel):
     __tablename__ = 'places'
-<<<<<<< HEAD
 
     title = db.Column(db.String(50), nullable=False)
     description = db.Column(db.String(256), nullable=False)
@@ -13,25 +12,6 @@ class Place(BaseModel):
     longitude = db.Column(db.Float)
     user_id = db.Column(db.String(36), db.ForeignKey('users.id'), nullable=False)
 
-    owner = relationship('User', back_populates='places', lazy=True)
-    reviews = relationship("Review", back_populates="place", lazy=True)
-    amenities = relationship("Amenity", secondary="place_amenity", back_populates="places", lazy=True)
-
-
-# Many-to-Many junction table
-class PlaceAmenity(BaseModel):
-    __tablename__ = 'place_amenity'
-
-    place_id = db.Column(db.String(36), db.ForeignKey('places.id'), primary_key=True)
-    amenity_id = db.Column(db.String(36), db.ForeignKey('amenities.id'), primary_key=True)
-=======
-
-    title = db.Column(db.String(50), nullable=False)
-    description = db.Column(db.String(256), nullable=False)
-    price = db.Column(db.Float)
-    latitude = db.Column(db.Float)
-    longitude = db.Column(db.Float)
-    owner_id = db.Column(db.String(36), db.ForeignKey('users.id'), nullable=False)
 
     owner = relationship('User', back_populates='places', lazy=True)
     reviews = relationship("Review", back_populates="place", lazy=True)
@@ -44,4 +24,3 @@ class PlaceAmenity(BaseModel):
 
     place_id = db.Column(db.String(36), db.ForeignKey('places.id'), primary_key=True)
     amenity_id = db.Column(db.String(36), db.ForeignKey('amenities.id'), primary_key=True)
->>>>>>> origin/master
