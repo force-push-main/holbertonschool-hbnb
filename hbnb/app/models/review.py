2dbe2e16f0317795dc49d5e2305d64509d972e8e
adashp02 <adashp02@gmail.com>
2025-08-08 14:17:11 +1000

    change

--- hbnb/app/models/review.py
+++ hbnb/app/models/review.py
@@ -15,11 +15,3 @@ class Review(BaseModel):
 
     author = relationship("User", back_populates="reviews", lazy=True)
     place = relationship("Place", back_populates="reviews", lazy=True)
-
-    text = db.Column(db.String(512), nullable=False)
-    rating = db.Column(db.Integer)
-    user_id = db.Column(db.String(36), db.ForeignKey('users.id'))
-    place_id = db.Column(db.String(36), db.ForeignKey('places.id'))
-
-    author = relationship("User", back_populates="reviews", lazy=True)
-    place = relationship("Place", back_populates="reviews", lazy=True)
